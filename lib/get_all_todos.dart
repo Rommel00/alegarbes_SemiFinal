ae5f7d045a7a020137a7d0b57172d54f7884b8cf
Rommel <mochizukisouta00@gmail.com>
2022-12-31 09:48:47 +0800

    add http and display todos

--- lib/get_all_todos.dart
+++ lib/get_all_todos.dart
@@ -1,4 +1,7 @@
+import 'dart:convert' as convert;
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class GetAllTodos extends StatefulWidget {
   const GetAllTodos({Key? key}) : super(key: key);
@@ -8,8 +11,38 @@ class GetAllTodos extends StatefulWidget {
 }
 
 class _GetAllTodosState extends State<GetAllTodos> {
+  /*init tod0 list*/
+  List todos = <dynamic>[];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("g"));
+    return ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index]['title']),
+            leading: CircleAvatar(
+              child: Text("${index + 1}"),
+            ),
+            onTap: () {},
+          );
+        });
+  }
+
+  /*get list from api*/
+  getTodo() async {
+    var url = 'https://jsonplaceholder.typicode.com/todos';
+    var response = await http.get(Uri.parse(url));
+
+    setState(() {
+      todos = convert.jsonDecode(response.body) as List<dynamic>;
+    });
+  }
+
+  /*init - get all tod0*/
+  @override
+  void initState() {
+    getTodo();
+    super.initState();
   }
 }
